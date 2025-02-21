0ab5e96632d3b2e869845a05d2e11cdb6b40c864
William Kermer <william.kermer21@gmail.com>
2025-02-21 15:02:46 -0300

    studing primitive types

---
+++ aula9/index.js
@@ -0,0 +1,17 @@
+//Disclaimer Let e Var
+
+var nome = "Luiz"; // var deve ser evitado pois pode redeclarar variavel e atrapalhar o código
+var nome = "Ótavio"; // NÃO FAÇA ISSO
+
+console.log(nome);
+
+//Tipos Primitivos
+
+const name = "Gica"; // String Type
+const num1 = 10; //Number type
+const num2 = 10.52; //Number type too
+let nomeAluno; // undefined -> não aponta pra lugar nenhum da memória
+let sobrenomeAluno = null; // nulo -> não aponta pra lugar nenhum da memória mas é setado pelo dev, diferente de nomeAluno
+const aprovado = true; //boolean type = true, false (lógico)
+
+console.log(typeof aprovado)
