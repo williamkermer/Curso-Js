7b1c704e0846bf19c2feef9dd0f2529121abf248
William Kermer <william.kermer21@gmail.com>
2025-02-21 21:56:04 -0300

    using prompt, alert and confirm

---
+++ aula11/js/index.js
@@ -0,0 +1,13 @@
+alert("Com a nossa mensagem.");
+
+const confirma = confirm("OK?");
+
+prompt("Diga o que pensa");
+
+console.log(confirma);
+
+if (confirma == false){
+
+    alert("canceled")
+
+}
