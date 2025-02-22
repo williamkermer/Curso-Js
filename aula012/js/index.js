b7c2a508cdd73b31e28275443a2d6dcbddd8d497
William Kermer <william.kermer21@gmail.com>
2025-02-21 23:35:04 -0300

    ex

---
+++ aula012/js/index.js
@@ -0,0 +1,9 @@
+let num1 = prompt("Digite um numero: ");
+let num2 = prompt("Digite outro numero: ");
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+const resultado = num1 + num2;
+
+alert("O resultado da sua conta foi: " + resultado);
