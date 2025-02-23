a2cac72add3cc91c7093407179787171fab3b4cb
William Kermer <william.kermer21@gmail.com>
2025-02-22 22:25:51 -0300

    simple challenge

---
+++ aula012/index.js
@@ -0,0 +1,11 @@
+let varA = 'A'; //B
+let varB = 'B'; //C
+let varC = 'C'; //A
+
+[varA, varB, varC] = [varB, varC, varA];
+
+//const varATemp = varA;
+//result = ((varA = varB) + (varB = varC) + (varC = varATemp))
+
+
+console.log(varA, varB, varCdesafio);
