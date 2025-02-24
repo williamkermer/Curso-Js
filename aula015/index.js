03dd8c00590f290bd05ef8aa3be2d47cc8403f9a
William Kermer <william.kermer21@gmail.com>
2025-02-24 17:03:56 -0300

    using math function

---
+++ aula015/index.js
@@ -0,0 +1,9 @@
+let num1 = 9.54578;
+//let num2 = Math.floor(num1);
+//let num2 = Math.ceil(num1);
+let num2 = Math.round(num1);
+
+console.log(Math.max(1,2,3,4,5,6,7,8,9,0));
+console.log(num2);
+console.log(Math.random());
+console.log(Math.PI);
