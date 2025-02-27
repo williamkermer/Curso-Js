dcbfd2ad0acdba57cc0156cac94b09961dfa909e
William Kermer <william.kermer21@gmail.com>
2025-02-27 12:20:41 -0300

    if and else in Js

---
+++ aula024/index.js
@@ -0,0 +1,19 @@
+const hora = 20;
+
+if (hora < 12){
+
+    console.log('bom dia');
+
+} else if (hora >= 12 && hora < 18){
+
+    console.log('boa tarde');
+
+}else if(hora >= 18 && hora <= 23){
+
+    console.log('boa noite');
+
+}else{
+
+    console.log('Oi')
+
+}
