f05d0607288ff54828fb48818a29ebba85ef285b
William Kermer <william.kermer21@gmail.com>
2025-02-27 11:37:44 -0300

    LOP study

---
+++ aula021/index.js
@@ -0,0 +1,21 @@
+/*
+Operadores de comparação
+> maior que
+>= igual ou maior que 
+< menor que 
+<= igual ou menor que
+== igualdade (valor)
+=== igualdade restrita (valor e tipo)
+!= diferente (valor)
+!== diferente restrito (valor e tipo)
+*/
+
+const num1 = 10;
+const num2 = '10';
+const comp = num1 === num2;
+
+console.log(comp);
+
+
+const comp2 = num1 !== num2;
+console.log(comp2);
