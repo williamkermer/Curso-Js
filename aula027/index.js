e33732a93b53b61b00b8f255d935c98ea6fb1c19
William Kermer <william.kermer21@gmail.com>
2025-03-01 16:41:55 -0300

    conditional (ternary) operator

---
+++ aula027/index.js
@@ -0,0 +1,18 @@
+//Operador ternário - ?:
+
+const pontuacaoUsuario = 9999;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário Vip' : 'Usuário Normal'
+//                   Variavel medida - condição - caso verdadeiro : caso falso
+
+console.log(nivelUsuario);
+
+
+// if(pontuacaoUsuario >= 1000){
+
+//     console.log('Usuário Vip');
+
+// }else{
+
+//     console.log('Usuario Normal');
+
+// }
