ee4948f37fba57b4732caf54f11732f874ba1144
William Kermer <william.kermer21@gmail.com>
2025-03-04 19:26:50 -0300

    For in

---
+++ aula036/index.js
@@ -0,0 +1,27 @@
+const frutas = ['pera', 'maçã', 'uva'];
+
+const pessoa = {
+
+    nome: 'Luiz',
+    sobrenome: 'Otavio',
+    idade: 30
+
+}
+
+for (i in pessoa){
+
+     console.log(pessoa[i]);
+
+ }
+
+// for(i = 0; i < frutas.length; i++){
+
+//     console.log(frutas[i])
+
+// }
+
+// for (i in frutas){
+
+//     console.log(frutas[i]);
+
+// }
