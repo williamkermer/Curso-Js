0127aa63895db1e7e5eff61dc34fb738804ee990
William Kermer <william.kermer21@gmail.com>
2025-03-04 19:47:40 -0300

    For of

---
+++ aula037/index.js
@@ -0,0 +1,13 @@
+const nome = 'Luiz Otavio';
+
+for(let valor of nome){
+
+    console.log(valor);
+
+}
+
+/*
+For - Geralmente com iteráveis
+For in - Retornao índice ou a chave
+For of - Retorna o valor em si (iteráveis)
+*/
