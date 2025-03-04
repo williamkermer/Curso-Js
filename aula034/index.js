eff035538f605339ab6fadf57753eb81ab079d48
William Kermer <william.kermer21@gmail.com>
2025-03-03 22:06:16 -0300

    'For' uses

---
+++ aula034/index.js
@@ -0,0 +1,13 @@
+const frutas = ['Maçã', 'Pera', 'Uva'];
+
+for(let i = 0; i < frutas.length; i++){
+
+    console.log(`Indice ${i} ${frutas[i]}`);
+
+}
+// for(let i = 1; i <= 10; i++){
+
+//     const par = i % 2 === 0 ? 'Par' : 'Impar';
+//     console.log(i, par);
+
+// }dasd
