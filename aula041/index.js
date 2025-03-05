dbb6df8d49e588f136c4638986c92aee28b6d159
William Kermer <william.kermer21@gmail.com>
2025-03-05 13:33:31 -0300

    simple function

---
+++ aula041/index.js
@@ -0,0 +1,10 @@
+//Escreva uma função que recebe 2 números e retorne o maior deles
+
+// const maior = (x, y) =>  x < y ? x : y;    -> Arrow Function
+
+function maior(x, y){
+
+    return x > y ? x : y;
+}
+ 
+console.log(maior(2, 1000));
