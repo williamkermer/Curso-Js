ff07dfe91ff25b8bdbb6d2c72a5e8c7478cd38d5
William Kermer <william.kermer21@gmail.com>
2025-03-05 14:39:09 -0300

    Tratando e lançando erros pt2 (try, catch, finally)

---
+++ aula045/index.js
@@ -0,0 +1,53 @@
+function retornaHora(data){
+
+    if (data && !(data instanceof Date)){
+
+        throw new TypeError('Esperando instancia de Date.');
+    }
+
+    if (!data){
+
+        data = new Date();
+
+    }
+
+
+    return data.toLocaleTimeString('pt-BR', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',        
+        hour12: false
+    });
+
+}
+
+try{
+    const data = new Date(0);
+    const hora = retornaHora(11);
+    console.log(hora);
+}catch(e){
+    //Tratar Erro
+}finally{
+
+    console.log('tenha um bom dia');
+
+}
+
+
+
+
+// try{//É executado quando não há erros
+
+//     console.log('Abri um arquivo');
+//     console.log('Manipulei o arquivo e gerou erro');
+//     console.log('Fechei o arquivo');
+
+// }catch(e){//É executada quando há erros
+
+//     console.log('Tratando o erro');
+
+// }finally{//Sempre é executada
+
+//     console.log('Tenta fechar o arquivo para ter certeza de sucesso');
+
+// }
