a8053fdd4be205ddb0d4a6bfe5ca8b0bcb53e3c4
William Kermer <william.kermer21@gmail.com>
2025-03-05 14:41:11 -0300

    fix layout

--- aula045/index.js
+++ aula045/index.js
@@ -1,3 +1,19 @@
+// try{//É executado quando não há erros
+
+//     console.log('Abri um arquivo');
+//     console.log('Manipulei o arquivo e gerou erro');
+//     console.log('Fechei o arquivo');
+
+// }catch(e){//É executada quando há erros
+
+//     console.log('Tratando o erro');
+
+// }finally{//Sempre é executada
+
+//     console.log('Tenta fechar o arquivo para ter certeza de sucesso');
+
+// }
+
 function retornaHora(data){
 
     if (data && !(data instanceof Date)){
@@ -32,22 +48,3 @@ try{
     console.log('tenha um bom dia');
 
 }
-
-
-
-
-// try{//É executado quando não há erros
-
-//     console.log('Abri um arquivo');
-//     console.log('Manipulei o arquivo e gerou erro');
-//     console.log('Fechei o arquivo');
-
-// }catch(e){//É executada quando há erros
-
-//     console.log('Tratando o erro');
-
-// }finally{//Sempre é executada
-
-//     console.log('Tenta fechar o arquivo para ter certeza de sucesso');
-
-// }
