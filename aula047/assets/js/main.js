d0418238f52585c502d8c3438a99e1aaae6f411a
William Kermer <william.kermer21@gmail.com>
2025-03-06 15:03:46 -0300

    Timer Page

---
+++ aula047/assets/js/main.js
@@ -0,0 +1,78 @@
+function segs(segundos){
+    
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-Br', {
+        hour12: false,
+        timeZone: 'GMT'
+    });
+
+}
+
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar');
+let segundos = 0;
+let timer;
+
+
+function iniciarRelogio(){
+
+    timer = setInterval(function(){
+        segundos++;
+        relogio.innerHTML = segs(segundos);
+    }, 1000);
+}
+
+document.addEventListener('click', function(e){
+
+    const el = e.target;
+
+    if(el.classList.contains('iniciar')){
+
+        relogio.classList.remove('pausado');
+        clearInterval(timer);
+        iniciarRelogio();
+
+    }
+
+    if(el.classList.contains('pausar')){
+
+        clearInterval(timer);
+        relogio.classList.add('pausado');
+
+    }
+
+    if(el.classList.contains('zerar')){
+
+        clearInterval(timer); 
+        relogio.innerHTML = '00:00:00'
+        segundos = 0;
+        relogio.classList.remove('pausado');
+
+    }
+
+
+})
+
+// iniciar.addEventListener('click', function(event){
+
+//     relogio.classList.remove('pausado');
+//     clearInterval(timer);
+//     iniciarRelogio();
+
+// });
+// pausar.addEventListener('click', function(event){
+
+//     clearInterval(timer);
+//     relogio.classList.add('pausado');
+
+// });
+// zerar.addEventListener('click', function(event){
+
+//     clearInterval(timer); 
+//     relogio.innerHTML = '00:00:00'
+//     segundos = 0;
+//     relogio.classList.remove('pausado');
+
+// });
